60b2ab85fa9d53346458e50ad158496127f03bee
SGreen24 <113150613+SGreen24@users.noreply.github.com>
2024-09-28 17:14:49 -0400

    Added Questionnaire Page

--- hackumbc/src/main.jsx
+++ hackumbc/src/main.jsx
@@ -1,17 +1,20 @@
 // src/main.jsx
-import React from "react";
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import React Router components
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home"; // Import Home component
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from './components/Login/Auth'; // Import Auth component
+import Home from './components/Home/Home'; // Import Home component
+import Questionnaire from './components/Questionnaire/Questionnaire'; // Import Questionnaire component
+import './index.css'; // Import global styles
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Router> {/* Wrap the app in the Router component */}
-      <Routes> {/* Define the routes for your app */}
-        <Route path="/" element={<Login />} /> {/* Login page is the default route */}
-        <Route path="/home" element={<Home />} /> {/* Home page route */}
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Router> {/* Wrap the app in Router for routing */}
+      <Routes> {/* Define the routes */}
+        <Route path="/" element={<Auth />} /> {/* Default path renders the Auth component */}
+        <Route path="/home" element={<Home />} /> {/* /home path renders the Home component */}
+        <Route path="/questionnaire" element={<Questionnaire />} /> {/* /questionnaire path renders the Questionnaire component */}
       </Routes>
     </Router>
-  </React.StrictMode>
+  </StrictMode>
 );
