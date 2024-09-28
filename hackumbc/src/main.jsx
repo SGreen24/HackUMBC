2de9020538a92779c73ffe47e82d7311d937cffc
emma-h1 <90633343+emma-h1@users.noreply.github.com>
2024-09-28 18:20:53 -0400

    project button

--- hackumbc/src/main.jsx
+++ hackumbc/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from './components/Login/Auth'; // Import Auth component
 import Home from './components/Home/Home'; // Import Home component
 import Questionnaire from './components/Questionnaire/Questionnaire'; // Import Questionnaire component
+import Project from './components/Project/Project'; // Import Questionnaire component
 import './index.css'; // Import global styles
 
 createRoot(document.getElementById('root')).render(
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<Auth />} /> {/* Default path renders the Auth component */}
         <Route path="/home" element={<Home />} /> {/* /home path renders the Home component */}
         <Route path="/questionnaire" element={<Questionnaire />} /> {/* /questionnaire path renders the Questionnaire component */}
+        <Route path="/project" element={<Project />} /> {/* /questionnaire path renders the Questionnaire component */}
       </Routes>
     </Router>
   </StrictMode>
