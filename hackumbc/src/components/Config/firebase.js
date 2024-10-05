eb89bfc0e0f45c12935ac6b26432328371af83c2
SGreen24 <113150613+SGreen24@users.noreply.github.com>
2024-10-05 15:59:59 -0400

    Gatekeeping the keys bc i dont want hackers lol

--- hackumbc/src/components/Config/firebase.js
+++ hackumbc/src/components/Config/firebase.js
@@ -1,23 +1,15 @@
+// firebase.js
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC2cbKuXV_oHOATeRy7EdSube8C4cAjFjc",
-  authDomain: "hackumbc-9cd60.firebaseapp.com",
-  projectId: "hackumbc-9cd60",
-  storageBucket: "hackumbc-9cd60.appspot.com",
-  messagingSenderId: "617320581493",
-  appId: "1:617320581493:web:3e73fdea5e1fdb2a0d2b3c",
-  measurementId: "G-4B2FVL1GLV"
-};
+import { firebaseConfig } from './firebaseConfig'; // Import config from the secure file
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const googleProvider = new GoogleAuthProvider();
 export const auth = getAuth(app);
-export const db = getFirestore(app);  // Make sure you're using this for Firestore
-export const storage = getStorage(app);
+export const db = getFirestore(app);  // Firestore setup
+export const storage = getStorage(app);  // Storage setup
